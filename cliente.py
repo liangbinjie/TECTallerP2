c4aa820b8b81638f7ed0947d7cfc7288520bc504
liangbinjie <mailandresliang@gmail.com>
2023-04-01 23:48:23 -0600

    Update cliente.py
    Modified getClientes function to just one parameter, get the list from the getClientes function
    Added verify client existence function

--- cliente.py
+++ cliente.py
@@ -11,19 +11,20 @@ def getClientes():
 
 
 # Funcion para buscar un cliente por id
-def getCliente(idCliente, listaClientes):
-    for cliente in listaClientes:
+def getCliente(idCliente):
+    for cliente in getClientes():
         if cliente[0] == idCliente:
             # print(cliente)
             return cliente
     print("No se encontro el cliente")
     return False
-# getCliente("id", getClientes())
+# getCliente("id")
 
 
 # Funcion para eliminar un cliente de la base de datos
 def deleteCliente(cliente):
     if cliente == False:
+        print("No existe este cliente")
         return False
     with open("database/clientes.txt", "r") as f:
         lines = f.readlines()
@@ -32,22 +33,21 @@ def deleteCliente(cliente):
             if line.strip("\n").split(';') != cliente:
                 f.write(line)
     print("Cliente eliminado")
-# deleteCliente(getCliente("id", getClientes()))
+# deleteCliente(getCliente("id"))
 
 
 # Funcion para agregar un cliente
 def addCliente():
     # id autoincremental? || hacer funcion para actualizar los ids || o obtener ultimo id
     # cedula?
-    time = "" # importamos una libreria para obtener el dia
-    idCliente = input("Ingrese el id del cliente: ")
+    idCliente = clienteExist() # verificar que no este repetido
     nombreCliente = input("Ingrese el nombre del cliente: ")
     direccion = input("Ingrese el direccion del cliente: ")
-    ciudad = input("Ingrese el ciudad del cliente: ")
+    ciudad = input("Ingrese el ciudad del cliente: ") # verificar que la ciudad exista
     telefono = input("Ingrese el telefono del cliente: ")
-    fecha = time
-    descuento = input("Ingrese el porcentaje de descuento para el cliente: ")
-    saldo = input("Ingrese el saldo a deber del cliente: ")
+    fecha = input("Ingrese el dia de hoy: ")
+    descuento = int(input("Ingrese el porcentaje de descuento para el cliente: "))
+    saldo = int(input("Ingrese el saldo a deber del cliente: "))
     with open('prueba.txt', 'a') as f:
         nuevo = f'\n{idCliente};{nombreCliente};{direccion};{ciudad};{telefono};{fecha};{descuento};{saldo}'
         f.write(nuevo)
@@ -72,3 +72,20 @@ def modificarCliente(cliente):
                 f.write(f'3;{nombre};11;23;230323;0%;0\n')
     print("Cliente modificado")
 # modificarCliente(getCliente("id", getClientes()))
+
+
+# Funcion para verificar si el id del cliente existe, si no existe, retorna el valor del id
+def clienteExist():
+    encontrado = False
+
+    idCliente = input("Ingrese el id del cliente: ")
+    for cliente in getClientes():
+        if cliente[0] == idCliente:
+            encontrado = True
+    
+    if encontrado == True:
+        print("ID de cliente ya existe, ingrese otro")
+        return clienteExist()
+
+    else:
+        return idCliente
