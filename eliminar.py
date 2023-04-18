01f80a7181100d3d99c1ad74d6e286fa3b9a9cba
liangbinjie <mailandresliang@gmail.com>
2023-04-18 16:17:39 -0600

    Create eliminar.py

---
+++ eliminar.py
@@ -0,0 +1,27 @@
+from pais import deletePais, getPais
+from os import system
+
+
+def menuEliminar():
+    running = True
+
+    while running:
+        opcion = input("Has ingresado al modulo de eliminar, que deseas eliminar\n[1]Pais\n[2]Ciudad\n[3]Cliente\n[4]Mascotas\n> ")
+        if opcion == "1":
+            idPais = input("Ingrese el id del pais: ")
+            deletePais(getPais(idPais))
+        elif opcion == "2":
+            pass
+            # modificarCiudad()
+        elif opcion == "3":
+            pass
+            # addCliente()
+        elif opcion == "4":
+            pass
+            # addMascota()
+        elif opcion == "Q":
+            running = False
+        else:
+            print("Opcion invalida")
+        input(">...Presione enter")
+        system('cls')   
