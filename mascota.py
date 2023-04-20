2f57078dea5b47fc0b6054ec4bf6b88fdb232d86
liangbinjie <mailandresliang@gmail.com>
2023-04-19 21:05:18 -0600

    Update mascota.py

--- mascota.py
+++ mascota.py
@@ -37,7 +37,7 @@ def addMascota():
     # cliente exista
     # id mascota no repetido
     # NumCliente;IdAnimal;Nombre;tipomascota;raza;fechanacimiento;sexo;color; castrado; fechaultimavisita
-    idCliente = getCliente(input("Ingrese el id del cliente: "))
+    idCliente = getCliente(int(input("Ingrese el id del cliente: ")))
     while idCliente == False:
         idCliente = getCliente(input("Ingrese el id del cliente: "))
     
@@ -63,7 +63,7 @@ def modificarMascota():
 def mascotaExist():
     encontrado = False
 
-    idMascota = input("Ingrese el id de la mascota: ")
+    idMascota = int(input("Ingrese el id de la mascota: "))
     for mascota in getMascotas():
         if mascota[1] == idMascota:
             encontrado = True
