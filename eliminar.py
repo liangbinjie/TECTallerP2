664a3ff3c2175fab6f63d79cbe54839e5a19eb25
Suns3tz <129646390+Suns3tz@users.noreply.github.com>
2023-04-25 19:01:38 -0600

    Add files via upload

--- eliminar.py
+++ eliminar.py
@@ -1,12 +1,12 @@
 from pais import deletePais, getPais
 from os import system
-
+from medicacion import eliminaMed
 
 def menuEliminar():
     running = True
 
     while running:
-        opcion = input("Has ingresado al modulo de eliminar, que deseas eliminar\n[1]Pais\n[2]Ciudad\n[3]Cliente\n[4]Mascotas\n> ")
+        opcion = input("Has ingresado al modulo de eliminar, que deseas eliminar\n[1]Pais\n[2]Ciudad\n[3]Cliente\n[4]Mascotas\n[5]Medicacion\n> ")
         if opcion == "1":
             idPais = input("Ingrese el id del pais: ")
             deletePais(getPais(idPais))
@@ -19,6 +19,8 @@ def menuEliminar():
         elif opcion == "4":
             pass
             # addMascota()
+        elif opcion == "5":
+            eliminaMed()
         elif opcion == "Q":
             running = False
         else:
