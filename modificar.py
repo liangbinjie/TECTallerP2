8e6c722324a513bf715d262c3514b3eda30c9579
Suns3tz <129646390+Suns3tz@users.noreply.github.com>
2023-04-25 19:02:17 -0600

    Add files via upload

--- modificar.py
+++ modificar.py
@@ -1,12 +1,15 @@
+from mascota import addMascota
 from pais import modificarPais, getPais
+from visitas import Modvisita
 from os import system
-
+from tratamiento import modTratamientos
+from medicacion import modMedicamento
 
 def menuModificar():
     running = True
 
     while running:
-        opcion = input("Has ingresado al modulo de modificacion, que deseas modificar\n[1]Pais\n[2]Ciudad\n[3]Cliente\n[4]Mascotas\n> ")
+        opcion = input("Has ingresado al modulo de modificacion, que deseas modificar\n[1]Pais\n[2]Ciudad\n[3]Cliente\n[4]Mascotas\n[5]Visitas\n[6]Tratamientos\n[7]Medicacion\n> ")
         if opcion == "1":
             idPais = input("Ingrese el id del pais: ")
             modificarPais(getPais(idPais))
@@ -19,6 +22,12 @@ def menuModificar():
         elif opcion == "4":
             pass
             # addMascota()
+        elif opcion == "5":
+            Modvisita()
+        elif opcion == "6":
+            modTratamientos()
+        elif opcion == "7":
+            modMedicamento()
         elif opcion == "Q":
             running = False
         else:
